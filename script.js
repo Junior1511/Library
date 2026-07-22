43a7dbff21b5b17d1c72db38dcda20bd444fe9d2
Junior1511 <yunior122000@gmail.com>
2026-07-22 13:19:55 -0500

    Step 1 & 2 are fully implemented, also I am going to try to ge this done today

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const books = []
 
-function book() {
-    //Constructor goes here, might need to go over the lesson again.
+function Book(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+    this.id = crypto.randomUUID()
 }
 
-// need a function that adds it to the books array. 
+function addBookToLibrary (title, author, read) {
+    let newBook = new Book(title, author, read)
+    books.push(newBook)
+}
+
+addBookToLibrary("idk1","idk1","no")
+addBookToLibrary("idk2","idk2","yes")
+
+console.log(books)
+// need a function to loop through the books array and
+// display them ig
